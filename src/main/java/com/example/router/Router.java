4246e35f1e64df8482acae5f784b4a4778b71a65
imwower <george674834080@gmail.com>
2021-01-21 10:53:24 +0800

    match router

--- src/main/java/com/example/router/Router.java
+++ src/main/java/com/example/router/Router.java
@@ -14,28 +14,46 @@ public class Router {
         root.segment = "/";
     }
 
-    public void addRoute(String path) {
+    public Router addRoute(String path) {
         if (!StringUtils.isEmpty(path)) {
-            path = StringUtils.strip(path, "/");
-            String[] strings = StringUtils.split(path, "/");
+            String strippedPath = StringUtils.strip(path, "/");
+            String[] strings = StringUtils.split(strippedPath, "/");
             if (strings.length != 0) {
                 Node node = root;
                 for (int i = 0; i < strings.length; i++) {
                     String segment = strings[i];
                     node = addNode(node, segment);
                 }
+                node.path = path;
             }
         }
+        return this;
+    }
 
+    public String matchRoute(String path) {
+        if (!StringUtils.isEmpty(path)) {
+            String strippedPath = StringUtils.strip(path, "/");
+            String[] strings = StringUtils.split(strippedPath, "/");
+            if (strings.length != 0) {
+                Node node = root;
+                for (int i = 0; i < strings.length; i++) {
+                    String segment = strings[i];
+                    node = matchNode(node, segment);
+                    if (node == null)
+                        break;
+                }
+                if (node != null)
+                    return node.path;
+            }
+        }
+        return null;
     }
 
     private Node addNode(Node node, String segment) {
         Node childNode = new Node();
         childNode.segment = segment;
 
-        if ("*".equals(segment)) {
-            node.singleWildcard = childNode;
-        } else if ("**".equals(segment)) {
+        if ("**".equals(segment)) {
             node.wildcard = childNode;
         } else if (segment.startsWith("{") && segment.endsWith("}")) {
             node.dynamicRouter = childNode;
@@ -50,13 +68,23 @@ public class Router {
         return childNode;
     }
 
+    private Node matchNode(Node node, String segment) {
+        if (node.staticRouters != null && node.staticRouters.containsKey(segment)) {
+            return node.staticRouters.get(segment);
+        }
+        if (node.dynamicRouter != null)
+            return node.dynamicRouter;
+        if (node.wildcard != null)
+            return node.wildcard;
+        return null;
+    }
 
-    public class Node {
+
+    private class Node {
         private String path;
         private String segment;
         private Map<String, Node> staticRouters;
         private Node dynamicRouter;
-        private Node singleWildcard;
         private Node wildcard;
     }
 }
--- src/main/java/com/example/router/RouterApplication.java
+++ src/main/java/com/example/router/RouterApplication.java
@@ -14,11 +14,20 @@ public class RouterApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Router router = new Router();
-        router.addRoute("/user/*");
-//        router.addRoute("/user");
+        router.addRoute("/user/**");
+        router.addRoute("/user");
         router.addRoute("/user/login");
         router.addRoute("/user/{userId}");
+        router.addRoute("/equipment/**");
+        router.addRoute("/user/bg/**");
 
         System.out.println("OK");
+
+        String route = router.matchRoute("/user/bg/**");
+        System.out.println(route);
+        route = router.matchRoute("/user/123");
+        System.out.println(route);
+        route = router.matchRoute("/user");
+        System.out.println(route);
     }
 }
